f56c64ebd13cd1a525f72f62b56abcc286662e5d
Eloi Charra <eloi.charra@etu.univ-grenoble-alpes.fr>
2022-03-18 12:00:45 +0100

    change home page

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from './router'
+import i18n from './i18n'
 
 loadFonts()
 
-createApp(App).use(router)
+createApp(App).use(i18n).use(i18n).use(router)
   .use(vuetify)
   .mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <h1 class="text-h3">oui</h1>
+    <v-divider></v-divider>
+    <v-row justify="center" class="cards">
+      <v-col v-for="item in items"
+             :key="item.title">
+        <v-card
+            max-width="500"
+            @click="redirect(item.link)"
+        >
+          <v-img
+              class="text-white align-end"
+              height="200px"
+              :src="item.image"
+              cover
+          >
+            <v-card-title>{{ item.title }}</v-card-title>
+          </v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import notesMDImage from '@/assets/md.png'
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      items: [
+        {
+          title: "Notes MD",
+          link: "https://www.md.eloichr.xyz",
+          image: notesMDImage,
+          description: "Markdown notes"
+        }
+      ]
+    }
+  },
+  methods: {
+    redirect(link) {
+      window.location.href = link
+    }
   }
 }
 </script>
+<style>
+.cards {
+  margin : 5vh 10% 0 10%;
+}
+</style>
